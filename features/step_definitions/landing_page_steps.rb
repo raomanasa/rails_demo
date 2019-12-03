87a64e787e00554628b8655dd538e6272b6cd2c6
your_name <your@email.com>
2019-12-03 14:17:26 +0100

    adds new feature tests with steps

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -11,3 +11,21 @@ When('I am on the index page') do
       Article.create!(hash)
     end
   end
+  
+  #Given("I click on {string}") do |string|
+   # pending # Write code here that turns the phrase above into concrete actions
+ # end
+
+  When("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on {string}") do |string|
+   pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on the {string} page") do |string|
+   pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  
--- features/user_can_see_list_of_articles.feature
+++ features/user_can_see_list_of_articles.feature
@@ -11,10 +11,12 @@ Feature: List articles on landing page
 
   Scenario: Viewing list of articles on the application's landing page
     When I am on the index page
-    Then I should see "A breaking news item"
+   Then I should see "A breaking news item"
     And I should see "Some breaking action"
     And I should see "Learn Rails 5"
     And I should see "Build awesome rails applications"
+
+    
 
   
    
